0709bf6db9f88278d4ca28a699ef6232fb838eb6
vudongbui <34791984+vudongbui@users.noreply.github.com>
2018-01-05 23:11:37 +0700

    Add files via upload

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -19,13 +19,14 @@ public class GameCanvas extends JPanel{
     Vector <BulletPlayer> vectorBulletPlayer;
     Vector <Square> vectorSquareSmall;
     Vector <Square> vectorSquareMedium;
-    Vector <BulletSquare> vectorBulletSquareMedium;
+//    Vector <BulletSquare> vectorBulletSquareMedium;
+    BulletSquare bulletSquareMedium;
 
     public int positionPlayerX = 180;
     public int positionPlayerY = 500;
 
     int countBulletPlayer;
-    int countBulletSquareMedium;
+//    int countBulletSquareMedium;
     int countSquareSmall;
     int countSquareMedium;
 
@@ -52,7 +53,7 @@ public class GameCanvas extends JPanel{
     private  void  square (){
         this.vectorSquareSmall = new Vector<>();
         this.vectorSquareMedium = new Vector<>();
-        this.vectorBulletSquareMedium = new Vector<>();
+//        this.vectorBulletSquareMedium = new Vector<>();
     }
     private  void  setupplayer() {
         this.player = new Player(positionPlayerX,positionPlayerY,"resources/player/straight.png");
@@ -87,58 +88,12 @@ public class GameCanvas extends JPanel{
         for (Square squareMedium : this.vectorSquareMedium) {
             squareMedium.render(this.graphics);
         }
-        for (BulletSquare bulletSquareMedium : this.vectorBulletSquareMedium) {
-            bulletSquareMedium.render(this.graphics);
-        }
+//        for (BulletSquare bulletSquareMedium : this.vectorBulletSquareMedium) {
+//            bulletSquareMedium.render(this.graphics);
+//        }
         this.repaint();
     }
     public void runAll() {
-
-//                for (int i = 0; i < 5; i++) {
-//                    gameCanvas.graphics.drawImage(this.gameCanvas.square1,gameCanvas.square1X,gameCanvas.square1Y,null);
-//                    gameCanvas.square1X = gameCanvas.square1X + 40;
-//                }
-//
-//        this.square1Y = this.square1Y + this.plus1Y;
-//        if (this.square1Y>=530) {
-//            this.square1Y = 30;
-//        }
-//
-//        this.square2X = this.square2X + 1;
-//        if (this.square2X >=395){
-//            this.square2X =0;
-//        }
-//
-//        this.explosionY = this.explosionY + 3;
-//        if (this.explosionY >= 595) {
-//            this.explosionY = 20;
-//        }
-//
-//        this.explosion3X = this.explosion3X + this.explosion3plusX;
-//        this.explosion3Y = this.explosion3Y + 2;
-//        if (this.explosion3X >=this.positionPlayerX){
-//            this.explosion3plusX = -2;
-//        }
-//        else {
-//            this.explosion3plusX = 2;
-//        }
-//        if (this.explosion3Y >= this.positionPlayerY) {
-//            this.explosion3Y = this.squareY +5;
-//            this.explosion3X = this.squareX +5;
-//        }
-//        this.explosion1X = this.explosion1X + this.explosion1plusX;
-//        this.explosion1Y = this.explosion1Y + 3;
-//        if (this.explosion1X >=this.positionPlayerX){
-//            this.explosion1plusX = -3;
-//        }
-//        else {
-//            this.explosion1plusX = 3;
-//        }
-//        if (this.explosion1Y >= this.positionPlayerY) {
-//            this.explosion1Y = this.square1Y +5;
-//            this.explosion1X = this.square1X +5;
-//        }
-
         if (this.countBulletPlayer ==20){
             BulletPlayer bulletPlayer = new BulletPlayer(this.player.x,this.player.y,"resources/player/player_bullet.png",3,0);
             this.vectorBulletPlayer.add(bulletPlayer);
@@ -148,8 +103,9 @@ public class GameCanvas extends JPanel{
         }
         if (this.countSquareSmall ==40){
             Random random = new Random();
-            int randomSquareSmall = random.nextInt(394);
-            Square squareSmall = new Square(randomSquareSmall,5,0,3,"resources/square/enemy_square_small.png");
+            int randomSquareSmallX = random.nextInt(394);
+            int randomSpeedSquareSmallY = random.nextInt(2)+2;
+            Square squareSmall = new Square(randomSquareSmallX,5,0,randomSpeedSquareSmallY,"resources/square/enemy_square_small.png");
             this.vectorSquareSmall.add(squareSmall);
             this.countSquareSmall = 0;
         }else {
@@ -157,17 +113,12 @@ public class GameCanvas extends JPanel{
         }
         if (this.countSquareMedium ==100){
             Random random = new Random();
-            int randomSquareMedium = random.nextInt(369);
-            Square squareMedium = new Square(randomSquareMedium,5,3,4,"resources/square/enemy_square_medium.png");
+            int randomSquareMediumX = random.nextInt(369);
+            int randomSpeedSquareMediumX = random.nextInt(2)+2;
+            int randomSpeedSquareMediumY = random.nextInt(2)+2;
+            Square squareMedium = new Square(randomSquareMediumX,5,randomSpeedSquareMediumX,randomSpeedSquareMediumY,"resources/square/enemy_square_medium.png");
             this.vectorSquareMedium.add(squareMedium);
             this.countSquareMedium = 0;
-            if (this.countBulletSquareMedium ==10){
-                BulletSquare bulletSquareMedium = new BulletSquare(this.squareMedium.x,5,"resources/square/enemy_square_bullet.png", 2);
-                this.vectorBulletSquareMedium.add(bulletSquareMedium);
-                this.countBulletSquareMedium = 0;
-            }else {
-                this.countBulletSquareMedium++;
-            }
         }else {
             this.countSquareMedium++;
         }
@@ -185,9 +136,10 @@ public class GameCanvas extends JPanel{
         for (Square squareMedium:this.vectorSquareMedium) {
             squareMedium.run1();
         }
-        for (BulletSquare bulletSquareMedium:this.vectorBulletSquareMedium) {
-            bulletSquareMedium.run();
-        }
+//        bulletSquareMedium.run();
+//        for (BulletSquare bulletSquareMedium:this.vectorBulletSquareMedium) {
+//            bulletSquareMedium.run();
+//        }
 
 
 
